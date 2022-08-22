18b9db7a1d97eb9ecd80bc5451d8b63cfefb3e4f
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2022-08-22 23:34:29 +0700

    Add multiauth user

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.pages.dashboard');
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+// User Route
+
+
+// Admin Route
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+    Route::namespace('Auth')->group(function () {
+        // Login Route
+        // Route::get('login', 'AuthenticatedSessionController@create')->name('login');
+        // Route::post('login', 'AuthenticatedSessionController@store')->name('adminlogin');
+    });
 });
