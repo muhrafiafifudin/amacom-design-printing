78e650a5acefdfbbfa261824fecccdd0cea699bb
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2022-09-05 09:12:52 +0700

    Update checkout user

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -12,11 +12,11 @@ class CheckoutController extends Controller
     {
         $transaction = new Transaction();
         $transaction->users_id = Auth::id();
-        $transaction->products_images = Auth::id();
+        // $transaction->products_images = Auth::id();
         $transaction->phone_number = $request->input('phone_number');
-        $transaction->products_id = Auth::id();
-        $transaction->products_qty = Auth::id();
-        $transaction->total = Auth::id();
+        // $transaction->products_id = Auth::id();
+        // $transaction->products_qty = Auth::id();
+        // $transaction->total = Auth::id();
         $transaction->order_number = rand(0000000000, 9999999999);
         $transaction->save();
     }
